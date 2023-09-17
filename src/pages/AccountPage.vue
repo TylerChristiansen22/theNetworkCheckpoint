437a510a811a40fefa1f8fb0839803a05cf8313b
TylerChristiansen22 <tyguy.tc@gmail.com>
2023-09-17 15:22:47 -0600

    Finished

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid bg-primary">
     <section class="row">
       <div class="col-4">
         <h3>Welcome {{ account.name }}</h3>
